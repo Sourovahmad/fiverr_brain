28d7b703f52e3ab2ad0868993b67405ccef142c3
sourovahmad <mdsourovahmad19@gmail.com>
2021-11-26 22:46:39 +0600

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="home">
+        <div class="container">
+            <div class="theBox">
+                <div class="topLine">
+                    <span>Connect to a wallet</span>
+                </div>
+
+                    <div class="main">
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>MetaMask</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="photo_2021-11-26_04-02-42-removebg-preview.png" alt="">
+                                </div>
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>Ledger With Metamask</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="photo_2021-11-26_04-02-42-removebg-preview.png" alt="">
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>Wallet connect</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="download-removebg-preview.png" alt="">
+                                </div>
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>NBA Top Shot</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="images.jpg" alt="">
+                                </div>
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>Tezos</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="1200px-Tezos_logo.svg.png" alt="">
+                                </div>
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>Coinbase Wallet</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="download.jpg" alt="">
+                                </div>
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>Bitski</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="download (1).jpg" alt="">
+                                </div>
+                        </button>
+                        <button type="button" class="box btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <div class="walletName">
+                                    <span>Roll</span>
+                                </div>
+                                <div class="icon">
+                                    <img src="download.png" alt="">
+                                </div>
+                        </button>
+                    </div>
+            </div>
+
+
+                <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <div class="topLine d-flex align-items-center justify-contents-between">
+                        <img src="photo_2021-11-26_04-02-42-removebg-preview.png" alt="">
+                        <select name="" id="">
+                            <option value="">Etherrun Mainnet</option>
+                        </select>
+                    </div>
+
+                    </div>
+                    <div class="modal-body">
+                        <img src="photo_2021-11-26_04-02-42-removebg-preview.png" alt="">
+                        <h2>Welcome Back!</h2>
+                        <p>The decentralize web awaits.</p>
+
+                        <form action="">
+                            <input type="password" placeholder="Password">
+                            <input type="text" placeholder="Phrase">
+                            <input type="submit" value="Unlock">
+                        </form>
+
+                        <p class="d-flex"> or <a href="">import using Secret recovery Phrase</a></p>
+                        <p class="d-flex"> Need Help? Contact <a href="">Meta mask Support</a></p>
+                    </div>
+
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
